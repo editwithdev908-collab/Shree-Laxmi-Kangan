6d213a57a21ec829a47ffb27c23d2dc94d895b96
editwithdev908-collab <editwithdev908@gmail.com>
2026-06-19 21:45:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,84 @@ function orderProduct(productName) {
 
     window.open(whatsappURL, "_blank");
 }
+const products = [
+
+{
+name:"Bridal Bangles",
+price:"499",
+category:"bangles"
+},
+
+{
+name:"Fancy Bangles",
+price:"299",
+category:"bangles"
+},
+
+{
+name:"Lipstick",
+price:"199",
+category:"cosmetics"
+},
+
+{
+name:"Kajal",
+price:"99",
+category:"cosmetics"
+},
+
+{
+name:"Hair Clip",
+price:"149",
+category:"accessories"
+},
+
+{
+name:"Necklace Set",
+price:"799",
+category:"jewellery"
+},
+
+{
+name:"Designer Earrings",
+price:"249",
+category:"earrings"
+}
+
+];
+
+function showCategory(category){
+
+const container =
+document.getElementById("product-container");
+
+container.innerHTML="";
+
+products.forEach(product=>{
+
+if(category==="all" ||
+product.category===category){
+
+container.innerHTML += `
+<div class="product">
+
+<img src="https://picsum.photos/300">
+
+<h3>${product.name}</h3>
+
+<p>₹${product.price}</p>
+
+<button onclick="orderProduct('${product.name}')">
+Order Now
+</button>
+
+</div>
+`;
+
+}
+
+});
+
+}
+
+showCategory('all');
